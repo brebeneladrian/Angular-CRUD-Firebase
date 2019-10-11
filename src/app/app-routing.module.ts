d5ba91898852a54006cf2eaf924b376d3dd7a0c5
brebeneladrian <brebenel.adrian@yahoo.com>
2019-10-11 16:19:44 +0200

    dfsd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
+import { AddCourseComponent } from './add-course/add-course.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'course', component: CourseComponent },
+  { path: 'add-course', component: AddCourseComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
